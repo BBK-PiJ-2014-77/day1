185b5720a51632f7244c732a0b5be26db89fc493
Ambrose <a.cole@mail.cryst.bbk.ac.uk>
2014-10-06 19:48:28 +0100

    third commit day 1

---
+++ ex9.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+public class ex9
+{
+		private static Scanner scanner=new Scanner(System.in); static{scanner.useDelimiter(System.getProperty("line.separator"));}
+	public static void main(String[] args)
+	{
+//ask for first number and say end with -1
+//if -1 say please add at least 1 positive number
+//ask for another
+//have a flag that goes to false if new number not equal to i + 1
+//report flag status
+
+String inputnumber="";
+int n1=0, n2=0, i=0;
+boolean flag = true;
+
+ System.out.println ("please give first number");
+ inputnumber = System.console().readLine(); 
+ n1 = Integer.parseInt(inputnumber);
+ i = n1; 
+ while (n1 != -1 ) {
+ 	System.out.println ("and another");
+	inputnumber = System.console().readLine();
+	n1 = Integer.parseInt(inputnumber);
+	if (n1 != i + 1 && n1 != -1) {
+		flag = false;
+	}
+i = n1;
+	
+}
+System.out.println ("It is " + flag + " that this is a sequence" );
+}}
+
