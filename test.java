7af763eee85d7bba81e8a47f07fdb769020fcc5f
Ambrose <a.cole@mail.cryst.bbk.ac.uk>
2014-10-09 21:38:25 +0100

    ex11 commit

---
+++ test.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class test
+{
+		private static Scanner scanner=new Scanner(System.in); static{scanner.useDelimiter(System.getProperty("line.separator"));}
+	public static void main(String[] args)
+	{
+int cv = 1;
+String cardnumber = "";
+System.out.println ("please give value of card number ");
+cardnumber =  System.console().readLine(); 
+	if ( cardnumber == "j" ) {
+		cardnumber = "11";
+		}
+		
+cv = Integer.parseInt(cardnumber); 
+
+System.out.println (cv);
+}}
+
